bdb8ca229e2b49b3c4b6c9b1f0a9694dbcde216d
comedinhasss <comedinhasss@9acaf728-1f57-42ea-b718-c133e188366d>
2010-10-10 15:43:38 +0000

    Fixs por MapperNeto

--- trunk/data/movements/scripts/azerus.lua
+++ trunk/data/movements/scripts/azerus.lua
@@ -3,10 +3,12 @@ function onStepIn(cid, item, position, fromPosition)
 --Config-->
 local queststatus = getPlayerStorageValue(cid, 50001)
 --EndConfig-->
-	if item.actionid == 1974 and queststatus == -1 then
-		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "It seems by defeating Azerus you have stoped this army from entering your world! Better leave this ghastly place forever.")
-		setPlayerStorageValue(cid, 4765, 1)
-	return TRUE
+if item.actionid == 1974 and queststatus == -1 then
+doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "It seems by defeating Azerus you have stoped this army from entering your world! Better leave this ghastly place forever.")
+doPlayerAddOutfit(cid, 325, 2)
+doPlayerAddOutfit(cid, 324, 2)
+setPlayerStorageValue(cid, 4765, 1)
+return TRUE
 end
 
 if item.actionid == 1973 and queststatus == -1 then
@@ -19,20 +21,20 @@ totalmonsters=0
 monster = {}
 repeat
 creature= getThingfromPos(checking)
- if creature.itemid > 0 then
- if getPlayerAccess(creature.uid) == 0 then
- players=players+1
- end
-  if getPlayerAccess(creature.uid) ~= 0 and getPlayerAccess(creature.uid) ~= 3 then
- totalmonsters=totalmonsters+1
-  monster[totalmonsters]=creature.uid
-   end
- end
+if creature.itemid > 0 then
+if getPlayerAccess(creature.uid) == 0 then
+players=players+1
+end
+if getPlayerAccess(creature.uid) ~= 0 and getPlayerAccess(creature.uid) ~= 3 then
+totalmonsters=totalmonsters+1
+monster[totalmonsters]=creature.uid
+end
+end
 checking.x=checking.x+1
-  if checking.x>ending.x then
-  checking.x=starting.x
-  checking.y=checking.y+1
- end
+if checking.x>ending.x then
+checking.x=starting.x
+checking.y=checking.y+1
+end
 until checking.y>ending.y
 if players==0 then
 trash= {x=33193, y=31689, z=15}
