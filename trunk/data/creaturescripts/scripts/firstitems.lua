b499762807087c7f22a91187553473b0c8774334
comedinhasss <comedinhasss@9acaf728-1f57-42ea-b718-c133e188366d>
2010-10-23 12:19:56 +0000

    firstitems arrumados

--- trunk/data/creaturescripts/scripts/firstitems.lua
+++ trunk/data/creaturescripts/scripts/firstitems.lua
@@ -1,29 +1,72 @@
   local commonItems = {
   -- ITEMS ALL VOCS RECEIVE
-  {itemid=2480, count=1}, -- legion helmet
-  {itemid=2464, count=1}, -- chain armor
-  {itemid=2468, count=1}, -- studded legs
-  {itemid=2643, count=1}, -- leather boots
   {itemid=2120, count=1}, -- rope
-  {itemid=5710, count=1} -- shovel
+  {itemid=2175, count=1}, -- spellbook
+  {itemid=2160, count=1}, -- cristal coin
+  {itemid=5710, count=1}, -- shovel
+  {itemid=2789, count=5}, -- brown mushrooms
+  {itemid=2305, count=100}, -- fire bomb rune
+  {itemid=2261, count=100}, -- destroy field rune
 }
 
 local firstItems = {
   { -- SORC ITEMS
-    {itemid=2190, count=1}, -- wand of vortex
-    {itemid=2175, count=1} -- spellbook
+    {itemid=2323, count=1}, -- hat of the mad
+    {itemid=8871, count=1}, -- focus cape
+    {itemid=7730, count=1}, -- blue legs
+    {itemid=2195, count=1}, -- boots of haste
+    {itemid=8902, count=1}, -- spellbook of mind control
+    {itemid=2187, count=1}, -- wand of inferno  
+
+    {itemid=2268, count=100}, -- sd
+    {itemid=2273, count=100}, -- uh
+    {itemid=7590, count=100}, -- gmp
+    {itemid=2293, count=100}, -- mw
   },
   { -- DRUID ITEMS
-    {itemid=2182, count=1}, -- snakebite rod
-    {itemid=2175, count=1} -- spellbook
+    {itemid=2323, count=1}, -- hat of the mad
+    {itemid=8871, count=1}, -- focus cape
+    {itemid=7730, count=1}, -- blue legs
+    {itemid=2195, count=1}, -- boots of haste
+    {itemid=8902, count=1}, -- spellbook of mind control
+    {itemid=2183, count=1}, -- hailstorm rod  
+
+    {itemid=2268, count=100}, -- sd
+    {itemid=2273, count=100}, -- uh
+    {itemid=7590, count=100}, -- gmp
+    {itemid=2293, count=100}, -- mw
+    {itemid=2269, count=100}, -- wg
+    {itemid=2278, count=100}, -- para
   },
   { -- PALADIN ITEMS
-    {itemid=2456, count=1}, -- bow
-    {itemid=2544, count=100} -- 100 arrows
+    {itemid=2493, count=1}, -- demon helmet
+    {itemid=8891, count=1}, -- paladin armor
+    {itemid=7730, count=1}, -- blue legs
+    {itemid=2195, count=1}, -- boots of haste
+    {itemid=2514, count=1}, -- mastermind shield
+    {itemid=7368, count=100}, -- assassin stars
+
+    {itemid=2268, count=100}, -- sd
+    {itemid=2273, count=100}, -- uh
+    {itemid=8472, count=100}, -- gsp
+    {itemid=7589, count=100}, -- smp
+    {itemid=7588, count=100}, -- shp
+    {itemid=2293, count=100}, -- mw
   },
   { -- KNIGHT ITEMS
-    {itemid=2412, count=1}, -- katana
-    {itemid=2530, count=1} -- copper shield
+    {itemid=2493, count=1}, -- demon helmet
+    {itemid=2472, count=1}, -- magic plate armor
+    {itemid=2470, count=1}, -- golden legs
+    {itemid=2195, count=1}, -- boots of haste
+    {itemid=2514, count=1}, -- mastermind shield
+    {itemid=2400, count=1}, -- magic sword
+    {itemid=2431, count=1}, -- Stonecutter Axe
+
+    {itemid=7620, count=100}, -- mp
+    {itemid=7591, count=100}, -- ghp
+    {itemid=8473, count=100}, -- uhp
+    {itemid=2313, count=100}, -- explosion
+    {itemid=2293, count=100}, -- mw
   }
 }
 
@@ -33,21 +76,25 @@ for _, items in ipairs(firstItems) do
   end
 end
 
-local storage = 35353
 function onLogin(cid)
-        if getPlayerGroupId(cid) < 3 then
-        local receivedItems = getPlayerStorageValue(cid, storage)
-                if receivedItems == -1 then
-                --[[local backpack = ]]doPlayerAddItem(cid, 1988, 1)
-                local giveItems = firstItems[getPlayerVocation(cid)]
-                        if giveItems ~= nil then
-                        for _, v in ipairs(giveItems) do
-                                --doAddContainerItem(backpack, v.itemid, v.count or 1)
-                                doPlayerAddItem(cid, v.itemid, v.count or 1)
-                        end
-                        doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "You have recieved your first items!")
-                end
-                setPlayerStorageValue(cid, storage, 1)
+  if getPlayerGroupId(cid) < 2 then
+    local hasReceivedFirstItems = getPlayerStorageValue(cid, 67708)
+
+    if hasReceivedFirstItems == -1 then
+      --[[local backpack = ]]doPlayerAddItem(cid, 1988, 1)
+
+      local giveItems = firstItems[getPlayerVocation(cid)]
+
+      if giveItems ~= nil then
+        for _, v in ipairs(giveItems) do
+          --doAddContainerItem(backpack, v.itemid, v.count or 1)
+          doPlayerAddItem(cid, v.itemid, v.count or 1)
         end
-        return true
+
+        doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "You have recieved your equipment")
+        setPlayerStorageValue(cid, 67708, 1)  
+      end
+    end
+  end
+  return TRUE
 end 
